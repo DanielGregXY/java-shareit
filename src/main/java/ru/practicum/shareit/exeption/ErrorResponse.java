4e9f6f7d5520b62992d9210d62d7432937a58638
DanielGregXY <DizertirsDen1@yandex.ru>
2023-01-24 11:43:28 +0700

    initial

---
+++ src/main/java/ru/practicum/shareit/exeption/ErrorResponse.java
@@ -0,0 +1,13 @@
+package ru.practicum.shareit.exeption;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+   private String error;
+   private String description;
+   //
+}
+
